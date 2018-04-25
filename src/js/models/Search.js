3732299ee066d28305404b028a4dd61bab7be961
Dmitriy Kiselev <mail@kanmir.ru>
2018-04-25 22:58:22 +0300

    Added Search model

---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = 'a4d6bc0df88603e83e945c2b5a06c2d0';
+
+        try {
+            const res =  await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (e) {
+            alert(e);
+        }
+    }
+
+
+}
