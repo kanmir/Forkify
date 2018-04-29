22ae242d1e7f65ee0c8812a90ac8a1690ee10b5d
Dmitriy Kiselev <mail@kanmir.ru>
2018-04-30 01:26:37 +0300

    added Search model and searchView

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,46 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from "./views/base";
+//   Global state of the app
+// - Search object
+// - Current recipe object
+// - Shopping list object
+// - Liked recipes
 
-const search = new Search('pizza');
-search.getResults();
-console.log(search);
+const state = {};
+
+const controlSearch = async () => {
+    // 1. Get query from the view
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2. New search object and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI results
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        searchView.clearInput();
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+};
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
